09ad9db5b3081e4adfbe4294b5165df8cc88ccb4
Gleb <kalytagleb@gmail.com>
2026-03-15 13:29:12 +0100

    Added JS and CSS. Refactoring html file for large desktops. Adding basic functionality.

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleFilter(id) {
+  const body = document.getElementById(id);
+  const btn = body.previousElementSibling;
+
+  body.classList.toggle("open");
+  btn.classList.toggle("filter-open");
+}
+document.getElementById("priceBtn").classList.add("filter-open");
+
+function toggleSort(id) {
+  const menu = document.getElementById(id);
+  menu.classList.toggle("open");
+}
+
+document.addEventListener("click", (e) => {
+  ["sortMenu", "catMenu"].forEach((id) => {
+    const menu = document.getElementById(id);
+    if (
+      menu &&
+      !menu.previousElementSibling.contains(e.target) &&
+      !menu.contains(e.target)
+    ) {
+      menu.classList.remove("open");
+    }
+  });
+});
